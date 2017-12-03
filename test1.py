b4fbb5f09bcfaaeb99273ca04644d852ea3c59de
Line Stettler <line.stettler@students.fhnw.ch>
2017-12-03 22:34:24 +0100

    added method for writing to csv files, but problems weith utf8 encoding

--- test1.py
+++ test1.py
@@ -1,4 +1,8 @@
 import tweepy
+import sys  # import sys package, if not already imported
+import json
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
 
 def get_twitter_api():
     consumer_key = '1KUtpMdGPAJl8oBJQJZgIJ0tr'
@@ -9,17 +13,59 @@ def get_twitter_api():
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
+def mentiened_users_to_string(users):
+    str = ""
+    print len(users)
+    print users
+    if len(users) >= 0:
+        for user in users:
+            str += user["id_str"] + " "
+            str += user["screen_name"]+ " "
+            str += user["name"]+ " "
+            str += "-"
+    print type(str)
+    return str.encode('utf-8')
+
 api = get_twitter_api()
 
-csv = open(twitter_1.csv, "w")
-
+csv = open("twitter_1.csv", "wb")
+c =0
 for tweet in tweepy.Cursor(api.search,
                            q="nobillag",
                            count=100,
                            result_type="recent",
                            include_entities=True,
                            lang="de").items():
-    row = tweet.created_at + "," + tweet.text + "," + tweet.
-    print tweet.created_at, tweet.text
+    dic_entities = tweet.entities
+    mentiened_users = mentiened_users_to_string(dic_entities["user_mentions"])
+    print mentiened_users
+    #m = json.dumps(dic_entities["user_mentions"])
+    row = (tweet.created_at).strftime("%Y-%m-%d %H:%M") + "," +  \
+    str(tweet.id) + "," + \
+    tweet.text + "," + \
+    tweet.user.name + "," + \
+    tweet.user.screen_name + "," + \
+    str(tweet.user.friends_count) + "," + \
+    str(tweet.user.followers_count) + "," + \
+    str(tweet.user.favourites_count) + "," + \
+    tweet.user.location + "," + \
+    str(tweet.retweet_count) + "," + \
+    tweet.user.description + "," + \
+    str(tweet.user.statuses_count) + "," + \
+    str(tweet.in_reply_to_status_id) + "," + \
+    tweet.in_reply_to_screen_name + "," + \
+    mentiened_users + "\n"
+    #str(m) + "\n"
+    
+    c += 1
+    print c
+    row_utf = row.encode('utf-8')
+    csv.write(row_utf)
+
+
+
+
+
+
 
 
